61e17812896e9631704201fffa07d8dde3b90820
Pete <petethegreat@gmail.com>
2020-12-30 14:20:42 -0500

    flask working

--- fitbit_stuff/redirect_catcher_flask.py
+++ fitbit_stuff/redirect_catcher_flask.py
@@ -2,24 +2,32 @@
 
 import flask
 import logging
-@app
-class RedirectFlaskApp(object):
-    def __init__(self):
-        self._logger = logging.getLogger("fitbit_stuff.RedirectFlaskApp")
+from flask_api import FlaskAPI
 
 
 
+def setup_app():
+    app = FlaskAPI("flask_oauth2_catcher")
+
+    @app.route("/")
+    def hello():
+        return "hello, flask app is working"
+
+    return app
 
 
+def start_catcher():
+    app = setup_app()
+    app.run(port=8080)
 
 
-def await_callback(*args,**kwargs):
-    logger = logging.getLogger("fitbit_stuff.await_callback")
-    logger.debug("await_callback - starting")
-    check_interval = kwargs.get("check_interval", 0.2)
-    log_interval = kwargs.get("log_interval", 5.0)
-    timeout = kwargs.get("timeout", 120.0)
-    redirect_uri = kwargs.get("redirect_uri","blah")
+# def await_callback(*args,**kwargs):
+#     logger = logging.getLogger("fitbit_stuff.await_callback")
+#     logger.debug("await_callback - starting")
+#     check_interval = kwargs.get("check_interval", 0.2)
+#     log_interval = kwargs.get("log_interval", 5.0)
+#     timeout = kwargs.get("timeout", 120.0)
+#     redirect_uri = kwargs.get("redirect_uri","blah")
 
     # begin = time.clock()
     # elapsed = 0
@@ -31,3 +39,6 @@ def await_callback(*args,**kwargs):
 
     # this function may run in a thread.
     # Or maybe the timing/waiting goes in the main thread.
+
+if __name__ == "__main__":
+    start_catcher()
