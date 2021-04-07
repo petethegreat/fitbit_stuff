47e02d666abe178859eda13a1b23a44799852b09
Pete <petethegreat@gmail.com>
2021-04-06 21:20:32 -0400

    initial analysis commit

---
+++ fitbit_stuff/analysis.py
@@ -0,0 +1,6 @@
+"""Some quick aggregation and analysis on downloaded data"""
+
+class HeartrateConverter(object):
+    """convert heartrate timeseries data to parquet"""
+    def __init__(self):
+        pass
